6792d57a4baa2bdd884f012c50279855a6f2f249
Felipe <TADS.FELIPE@outlook.com>
2024-08-17 15:35:29 -0300

    inicio do projeto

---
+++ components/task.tsx
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+type TaskProps = {
+    title: string,
+    description?: string,
+    concluded: boolean,
+    onToggleComplete: () => void,
+    onDelete: () => void,
+}
+
+
+const task = ({ title, description, concluded, onToggleComplete, onDelete }: TaskProps) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.textContainer}>
+                <Text style={[styles.title, concluded && styles.concluded]}>{title}</Text>
+                {description ? <Text>{description}</Text> : null}
+            </View>
+            <TouchableOpacity onPress={onToggleComplete}>
+                <Text>{concluded ? 'UNDO' : 'CONCLUDED'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onDelete}>
+                <Text>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 10,
+        marginVertical: 5,
+        backgroundColor: '#E0554F',
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    textContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 12,
+        color: '#666',
+    },
+    concluded: {
+        textDecorationLine: 'line-through',
+        color: '#999',
+    },
+    concludedButton: {
+        marginHorizontal: 10,
+        padding: 10,
+        backgroundColor: '#007EE1',
+    },
+    deleteButton: {
+        padding: 10,
+        backgroundColor: '#E53935',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 14,
+    }
+})
+
+export default task;
